31e7ea7006ed70c8e746e728736b491f5bcf9cee
LucasEdwa <lucas.eduardo2070@gmail.com>
2024-11-12 23:08:08 +0100

    react-router-dom, @tanstack/query installed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Routes>
+        <Route path="/" element={<h1 className='text-red-700 text-center p-4'>Home</h1>} />
+        <Route path="/timeline" element={<h1 className='text-center'>Timeline</h1>} />
+        <Route path="/seasons" element={<h1>Seasons</h1>} />
+        <Route path="/seasons/:seasonId" element={<h1>Season Details</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+        <Route path="/profile/:userId" element={<h1>User Profile</h1>} />
+      </Routes>
+
     </>
   )
 }
